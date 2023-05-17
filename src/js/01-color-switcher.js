c93ec07a942869cdc7fc95fef6697a3ee558263f
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-05-17 16:16:58 +0200

    task - 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const body = document.body
+const buttons = document.querySelectorAll('button')
+const btnStart = buttons[0];
+const btnStop = buttons[1]
+let colorPicker;
+
+
+btnStart.addEventListener('click', onStartChangeColor)
+btnStop.addEventListener('click', onStopChangeColor)
+
+
+function onStartChangeColor(event) {
+    btnStart.disabled = true;
+    btnStart.setAttribute('disabled', true)
+    colorPicker = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+}
+
+function onStopChangeColor(event) {
+    btnStart.disabled = false;
+    clearInterval(colorPicker)
+}
